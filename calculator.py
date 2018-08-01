9022dadcd35ff3a55ee8cf4748ec34b9fc9d1c54
mythomasliu <274966585@qq.com>
2018-08-01 17:51:15 +0800

    ca1 2

--- calculator.py
+++ calculator.py
@@ -3,32 +3,43 @@ import sys
 
 def cal(x):
     b = 0
-    try:
-        a = int(x[1])
-        if type(a) != int:
-            raise TypeError
-    except:
-        print("Parameter Error")
-        return
-
-    a = a - 3500
-    if a > 80000:
-        b = a * 0.45 - 13505
-    
-    if a > 55000 and a <= 80000:
-        b = a * 0.35 - 5505
-    if a > 35000 and a <= 55000:
-        b = a * 0.3 - 2755
-    if a > 9000 and a <= 35000:
-        b = a * 0.25 - 1005
-    if a > 4500 and a <= 9000:
-        b = a * 0.2 - 555
-    if a > 1500 and a <= 4500:
-        b = a * 0.1 - 105
-    if a <= 1500 and a > 0:
-        b = a * 0.03
-
-    print("{:.2f}".format(b))
+    d = 0
+    for i in range(1,len(x)):
+        num , gongzi = x[i].split(':')
+        try:
+            a = int(gongzi)
+            c = int(num)
+            if type(a) != int and type(c) != int:
+                raise TypeError
+        except:
+            print("Parameter Error")
+            sys.exit()
+        f = a * 0.165
+        e = a - 3500 - f
+        if e > 80000:
+            b = e * 0.45 - 13505
+            d = a - b - f
+        if e > 55000 and e <= 80000:
+            b = e * 0.35 - 5505
+            d = a - b - f
+        if e > 35000 and e <= 55000:
+            b = e * 0.3 - 2755
+            d = a - b - f
+        if e > 9000 and e <= 35000:
+            b = e * 0.25 - 1005
+            d = a - b - f
+        if e > 4500 and e <= 9000:
+            b = e * 0.2 - 555
+            d = a - b - f
+        if e > 1500 and e <= 4500:
+            b = e * 0.1 - 105
+            d = a - b - f
+        if e <= 1500 and e > 0:
+            b = e * 0.03
+            d = a - b - f
+        if e <= 0:
+            d = a - f
+        print("{0}:{1:.2f}".format(c,d))
 
 
 
